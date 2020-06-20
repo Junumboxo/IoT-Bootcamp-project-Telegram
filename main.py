028b995849447ca860308876dec98ddfe30a5958
Tatiana Barbova <44647094+Junumboxo@users.noreply.github.com>
2020-06-20 12:30:25 +0300

    Update /help message and code optimization
    - added func get_last_record()
    - added to all functions which send message a new argument chat_id

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from logging import getLogger
-from telegram import Bot, Update
+from telegram import Bot, Update, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from reply_buttons import BUTTON_LAST_RECORD, BUTTON_HELP, BUTTON_FEEL, get_base_reply_keyboard
 from msg_buttons import CALLBACK_BUTTON_GOOD, CALLBACK_BUTTON_BAD, CALLBACK_BUTTON_SEND, CALLBACK_BUTTON_DISCARD, get_inline_keyboard_health, get_inline_keyboard_confirm
@@ -23,7 +23,6 @@ def keyboard_callback_handler(bot, update, chat_data = None, **kwargs):
     chat_id = update.effective_message.chat_id
 
     if data == CALLBACK_BUTTON_GOOD:
-        #we should obligotary update the database with the data about health
         bot.send_message(
             chat_id = chat_id,
             text = msg_feel_GOOD,
@@ -42,12 +41,13 @@ def keyboard_callback_handler(bot, update, chat_data = None, **kwargs):
             reply_markup = get_base_reply_keyboard(),   
         )
         #here we have to send the data to the server
+
     elif data == CALLBACK_BUTTON_DISCARD:
         bot.send_message(
             chat_id = chat_id,
             text = msg_feel_DISCARD,  
         )
-        do_feel(bot, update)
+        do_feel(bot, update, chat_id)
 
 @debug_requests
 def do_start(bot, update):
@@ -58,41 +58,49 @@ def do_start(bot, update):
     )
 
 @debug_requests
-def do_help(bot, update):
-    chat_id = update.effective_message.chat_id
+def get_last_record(bot, update, chat_id):
+    #later we will get this data from the database
+    #and we will have to check these values on thresholds
+    datetime = "Current datetime"
+    temperature = 27.67
+    humidity = 55.00
+
+    reply = 'Datetime of measurements - {},\nTemperature - {} °C,\nHumidity - {} %'.format(datetime, temperature, humidity)
+    bot.send_message(
+        chat_id = chat_id,
+        text = reply,
+        reply_markup = get_base_reply_keyboard(),
+    )
+
+@debug_requests
+def do_help(bot, update, chat_id):
     bot.send_message(
         chat_id = chat_id,
         text = msg_help,
+        parse_mode = ParseMode.HTML,
         reply_markup = get_base_reply_keyboard(),
     )
 
 #ideally we should ask for health if we have a row in a database without data about health, but for now let it be like this
 @debug_requests
-def do_feel(bot, update):
-    chat_id = update.effective_message.chat_id
+def do_feel(bot, update, chat_id):
     bot.send_message(
         chat_id = chat_id,
         text = msg_feel,
         reply_markup = get_inline_keyboard_health(),
     )
 
-#here we will have to optimize the code when we get access to the database
 @debug_requests
 def do_echo(bot, update):
+    chat_id = update.effective_message.chat_id
     text = update.message.text
-    chat_id = update.message.chat_id
+
     if text == BUTTON_LAST_RECORD:
-        reply = "t = 27, h = 50" #later we will get this data from the database
-        #and we will have to check these values on thresholds
-        bot.send_message(
-            chat_id = chat_id,
-            text = reply,
-            reply_markup = get_base_reply_keyboard(),
-        )
+        get_last_record(bot, update, chat_id)
     elif text == BUTTON_HELP:
-        do_help(bot, update)
+        do_help(bot, update, chat_id)
     elif text == BUTTON_FEEL:
-        do_feel(bot, update)
+        do_feel(bot, update, chat_id)
     else:
         reply = msg_echo
         bot.send_message(
@@ -115,12 +123,10 @@ def main():
     logger.info(f'Bot info: {info}')
 
     start_handler = CommandHandler("start", do_start)
-    help_handler = CommandHandler("help", do_help)
     message_handler = MessageHandler(Filters.text, do_echo)
     buttons_handler = CallbackQueryHandler(callback = keyboard_callback_handler, pass_chat_data = True)
 
     updater.dispatcher.add_handler(start_handler)
-    updater.dispatcher.add_handler(help_handler)
     updater.dispatcher.add_handler(message_handler)
     updater.dispatcher.add_handler(buttons_handler)
 
--- msg_texts.py
+++ msg_texts.py
@@ -1,7 +1,24 @@
 ﻿from msg_buttons import TITLES_FEEL
 
-msg_start = "Hello! \nI am your climate control bot. I can provide information about the clime in your home. I will also sometimes ask how you feel. For more info enter command /help"
-msg_help = "This is help."
+msg_start = "Hello! \nI am your climate control bot. I can provide information about the clime in your home. I will also sometimes ask how you feel. For more info click the button **Help**"
+msg_help = '''
+IoT Climate Control bot is connected to the device system in your room, which measures different characteristics of the climate in your room: temperature and humidity. The system is designed so that people with <b>respiratory difficulties</b> could have better control of their health. 
+
+This bot has <b>3 main functionalities</b>:
+1. To give user the possibility to see last measurements of temperature and humidity in room/house
+2. Whenever a new measurement appears, to ask user how they feel, in order to use these data later to find optimal climate conditions for the user
+3. To send notifications whenever a value passes its threshold so that user can react properly and prevent possible deterioration of health.
+
+Button <b>Last update</b> will send the last measurements of temperature and humidity and the datetime they were effectuated.
+
+Button <b>Ask me how I feel</b> is a trial one. Ideally, the bot has to ask the user how they feel automatically, when a new update in the database appears. However, until we link the bot to the database, this button will serve the same function.
+
+<i>Note</i>: after choosing a state, to send it to the server you have to click <b>Send data</b> in the next message. In case of the wrong choice, click <b>Discard</b> and in the new message choose the new state.
+
+Button <b>Help</b> can remind you of what this bot can do.
+
+Stay safe!
+'''
 msg_echo = "Sorry. I don't understand you."
 msg_feel = "How do you feel? Haven't you got any difficulties of breath?"
 msg_feel_GOOD = "You have chosen {}.\nGlad you're OK!".format(TITLES_FEEL['INLINE_BUTTON_GOOD'])
